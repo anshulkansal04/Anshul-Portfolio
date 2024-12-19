b00d80ec8de632b2461b29c68543496bb5c38066
Anshul Kansal <anshulkansal89@gmail.com>
2024-12-19 17:33:38 +0530

    Updated the resume

--- client/src/data/constants.js
+++ client/src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/anshulkansal04",
   resume:
-    "https://drive.google.com/file/d/1w0CzMjsfxXNj81mf0N0aD2AW0v6_u_7g/view",
+    "https://drive.google.com/file/d/1hVeVQfGgzoBg4gT1dGm-mep3GciCU6nX/view",
   linkedin: "https://www.linkedin.com/in/anshul-kansal-5740a2254/",
   twitter: "https://twitter.com/anshulkansal89",
   insta: "https://www.instagram.com/anshulkansal04/",
